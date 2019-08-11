005d2f0a8a699a4fe8d6b02a19c301c83b3966f3
hemantdhiman92 <hemant.dhiman92@gmail.com>
2019-08-11 14:17:11 +0530

    First commit

---
+++ test/test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
+        System.out.println("Hello World!\nPlease Enter your name: ");
+        String _name = scan.nextLine();
+        System.out.println("Nice to meet you "+_name);
+    }
+}
