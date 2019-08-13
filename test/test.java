1926b4f2f974fa0fe47dece9d618f9d3afa45e6f
hemantdhiman92 <hemant.dhiman92@gmail.com>
2019-08-13 21:19:42 +0530

    last

--- test/test.java
+++ test/test.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-public class test {
-    public static void main(String[] args) {
+public class test{
+    public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
-        String _name = scan.nextLine();
-        System.out.println("Hello World!"+_name);
+        System.out.print("Please Enter Name: ");
+        String name = scan.nextLine();
+        System.out.println("Nice To Meet You Mr. "+name);
         scan.close();
     }
 }
