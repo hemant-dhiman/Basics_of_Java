3ed3679a5f9f7ccf9b08c70044bbb248d84e0c10
hemantdhiman92 <hemant.dhiman92@gmail.com>
2019-08-21 23:56:29 +0530

    intelli_j

--- test/test.java
+++ test/test.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 public class test{
-    public static void main(String args[]){
+    public static void main(String []args){
         Scanner scan = new Scanner(System.in);
         System.out.print("Please Enter Name: ");
         String name = scan.nextLine();
         System.out.println("Nice To Meet You Mr. "+name);
         scan.close();
+
     }
 }
