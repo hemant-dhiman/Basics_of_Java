ff434fbd7e9947c24a780bc2d188105eb050e862
hemantdhiman92 <hemant.dhiman92@gmail.com>
2019-08-26 10:17:41 +0530

    OOPs_Classes

---
+++ OOPs/ClassesConcept_2.java
@@ -0,0 +1,12 @@
+public class ClassesConcept_2 {
+    public static void main(String[] args) {
+        AnotherClass obj = new AnotherClass();
+        obj.method(); //Accessing another class method
+    }
+}
+
+class AnotherClass{
+    public void method(){
+        System.out.println("accessing AnotherClass method");
+    }
+}
