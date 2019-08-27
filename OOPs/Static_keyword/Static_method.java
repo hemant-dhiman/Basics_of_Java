a6d2376488f9ffcfa3c769fa23f3410e1b9ae76f
HEMANT <hemant.dhiman92@gmail.com>
2019-08-27 11:23:33 +0530

    static method added

--- OOPs/Static_keyword/Static_method.java
+++ OOPs/Static_keyword/Static_method.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 
+/*
+Java static method
+If you apply static keyword with any method, it is known as static method.
+
+A static method belongs to the class rather than the object of a class.
+A static method can be invoked without the need for creating an instance of a class.
+A static method can access static data member and can change the value of it.
+ */
+
 class Teacher{
     static int id = 1180;
 
