708fe3fa65cf992e7a992222125c43249821ede3
HEMANT <hemant.dhiman92@gmail.com>
2019-08-27 11:23:02 +0530

    static method added

--- OOPs/Static_keyword/Static_method.java
+++ OOPs/Static_keyword/Static_method.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
+class Teacher{
+    static int id = 1180;
+
+    static void print(){
+        System.out.println("current id:"+id);
+    }
+
+    static void change(int num){
+        id = num;
+        System.out.println("Updated id: "+id);
+    }
+}
+
 public class Static_method {
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+        Teacher.print(); // first printing id
+        System.out.println("If Need to update press  y otherwise n: ");
+        if(sc.nextLine().equalsIgnoreCase("y")){
+            System.out.println("Enter new id: ");
+            Teacher.change(sc.nextInt());
+        }else{
+            System.out.println("Thank you!");
+        }
+
+    }
 }
